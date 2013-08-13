247c3fab858b028ec2bc9c5091cef8a71040f6f4
takatsugu ishikawa <tissi0708@gmail.com>
2013-08-14 01:06:12 +0900

    エラーハンドリング処理を追加

--- app/controllers/Results.java
+++ app/controllers/Results.java
@@ -2,12 +2,14 @@ package controllers;
 
 import com.avaje.ebean.Ebean;
 import com.avaje.ebean.SqlRow;
+import com.avaje.ebean.TxRunnable;
 import models.Question;
 import models.Room;
 import models.Score;
 import models.ScorePK;
 import play.data.DynamicForm;
 import play.data.Form;
+import play.db.ebean.Model;
 import play.db.ebean.Model.Finder;
 import play.mvc.Controller;
 import play.mvc.Result;
@@ -25,22 +27,69 @@ public class Results extends Controller {
         String[] params = {"result_id", "score"};
         DynamicForm input = Form.form();
         input = input.bindFromRequest(params);
-        Map<String, String> data = input.data();
-        long result_id = Long.parseLong(data.get("result_id"));
-        int score = Integer.parseInt(data.get("score"));
+        final Map<String, String> data = input.data();
+        final long result_id = Long.parseLong(data.get("result_id"));
+        final int score = Integer.parseInt(data.get("score"));
 
-        Finder<Long, models.Result> finder =
-                new Finder<Long, models.Result>(Long.class, models.Result.class);
-        models.Result result = finder.byId(result_id);
-        result.score = score;
-        result.save();
+        try {
+            Ebean.execute(new TxRunnable() {
 
-        Map<String, Map> subjectCodeMap = new HashMap<String, Map>();
+                @Override
+                public void run() {
+                    Finder<Long, models.Result> finder =
+                            new Finder<Long, models.Result>(Long.class, models.Result.class);
+                    models.Result result = finder.byId(result_id);
+                    result.score = score;
+                    result.save();
+
+                    Map<String, Map> subjectCodeMap = makeScoreData(data);
+
+                    Set<String> subject_codes = subjectCodeMap.keySet();
+                    for (String subject_code : subject_codes) {
+                        ScorePK pk = new ScorePK();
+                        pk.result_id = result_id;
+                        pk.subject_code = subject_code;
+                        Score sc = new Score();
+                        sc.scorePK = pk;
+                        Map<String, Integer> map = subjectCodeMap.get(subject_code);
+                        sc.correct_number = map.get("correct_number");
+                        sc.mistake_number = map.get("mistake_number");
+                        sc.time_over_number = map.get("time_over_number");
+                        sc.total_number = map.get("total_number");
+                        sc.time = map.get("time");
+                        sc.save();
+                    }
+                }
+            });
+        }
+        catch (Exception e) {
+            postProc(result_id);
+            return ok(results.render(false));
+        }
+        postProc(result_id);
+        return ok(results.render(true));
+    }
+
+    private static void postProc(long result_id) {
+        int room_number = Questions.getRoomNumberByResultId(result_id);
+        Finder<Long, Room> roomFinder =
+                new Finder<Long, Room>(Long.class, Room.class);
+        Room room = roomFinder.byId(Long.valueOf(room_number));
+        if (room != null) {
+            room.status = true;
+            room.save();
+        }
+        // キャッシュしている問題リストの削除
+        Questions.deleteCacheByRoomNumber(room_number);
+    }
+
+    private static Map<String, Map> makeScoreData(Map<String, String> data) {
+        Map<String, Map> subjectScoreMap = new HashMap<String, Map>();
         for (int i = 1; i <= 20; i++) {
             String subject_code = data.get("answer" + i + "_subject_code");
             int status = Integer.parseInt(data.get("answer" + i + "_status"));
             int time = Integer.parseInt(data.get("answer" + i + "_time"));
-            Map<String, Integer> map = subjectCodeMap.get(subject_code);
+            Map<String, Integer> map = subjectScoreMap.get(subject_code);
             if (map == null) {
                 map = new HashMap();
                 map.put("correct_number", 0);
@@ -60,36 +109,9 @@ public class Results extends Controller {
             }
             map = updateMapContent(map, "total_number", 1);
             map = updateMapContent(map, "time", time);
-            subjectCodeMap.put(subject_code, map);
+            subjectScoreMap.put(subject_code, map);
         }
-
-        Set<String> subject_codes = subjectCodeMap.keySet();
-        for (String subject_code : subject_codes) {
-            ScorePK pk = new ScorePK();
-            pk.result_id = result_id;
-            pk.subject_code = subject_code;
-            Score sc = new Score();
-            sc.scorePK = pk;
-            Map<String, Integer> map = subjectCodeMap.get(subject_code);
-            sc.correct_number = map.get("correct_number");
-            sc.mistake_number = map.get("mistake_number");
-            sc.time_over_number = map.get("time_over_number");
-            sc.total_number = map.get("total_number");
-            sc.time = map.get("time");
-            sc.save();
-        }
-
-        int room_number = Questions.getRoomNumberByResultId(result_id);
-        Finder<Long, Room> roomFinder =
-                new Finder<Long, Room>(Long.class, Room.class);
-        Room room = roomFinder.byId(Long.valueOf(room_number));
-        room.status = true;
-        room.save();
-
-        // キャッシュしている問題リストの削除
-        Questions.deleteCacheByRoomNumber(room_number);
-
-        return ok(results.render(true));
+        return subjectScoreMap;
     }
 
     private static Map<String, Integer> updateMapContent(
